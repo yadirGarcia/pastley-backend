73dec6e50c4e00060c882a03ef46d2a79f52f493
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-10-23 15:44:14 -0500

    Product calculate values

--- pastley-sale/src/main/java/com/pastley/entity/Cart.java
+++ pastley-sale/src/main/java/com/pastley/entity/Cart.java
@@ -1,7 +1,6 @@
 package com.pastley.entity;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import javax.persistence.Column;
@@ -10,9 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import com.pastley.util.PastleyValidate;
 
 /**
  * @project Pastley-Sale.
@@ -63,15 +59,6 @@ public class Cart implements Serializable {
 	private boolean statu;
 
 	///////////////////////////////////////////////////////
-	// Other
-	///////////////////////////////////////////////////////
-	@Transient
-	private BigInteger priceDiscount;
-
-	@Transient
-	private BigInteger priceIva;
-
-	///////////////////////////////////////////////////////
 	// Builder
 	///////////////////////////////////////////////////////
 	public Cart() {
@@ -84,87 +71,6 @@ public class Cart implements Serializable {
 		this.price = price;
 	}
 
-	///////////////////////////////////////////////////////
-	// Method
-	///////////////////////////////////////////////////////
-	/**
-	 * 
-	 */
-	public void calculate() {
-		this.calculatePriceIva();
-		this.calculateSubTotalNeto();
-		this.calculateSubtotalGross();
-		this.calculateDiscount();
-	}
-
-	/**
-	 * 
-	 */
-	public void calculatePriceIva() {
-		this.priceIva = calculateToString(this.iva);
-	}
-
-	public void calculateSubTotalNeto() {
-		this.subtotalNet = BigInteger.ZERO;
-	}
-
-	public void calculateSubtotalGross() {
-		this.subtotalGross = BigInteger.ZERO;
-	}
-
-	/**
-	 * 
-	 */
-	public void calculateDiscount() {
-		this.priceDiscount = calculateToString(this.discount);
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public BigInteger calculatePriceAddPriceIva() {
-		if (!PastleyValidate.bigIntegerHigherZero(this.priceIva)) {
-			calculatePriceIva();
-		}
-		return (PastleyValidate.bigIntegerHigherZero(this.price)) ? this.price.add(this.priceIva) : BigInteger.ZERO;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public BigInteger calculatePriceSubDiscount() {
-		if(!PastleyValidate.bigIntegerHigherZero(this.priceDiscount)) {
-			calculateDiscount();
-		}
-		return (PastleyValidate.bigIntegerHigherZero(this.price)) ? this.price.subtract(this.priceDiscount) : BigInteger.ZERO;
-	}
-
-	public static void main(String... args) {
-		Cart cart = new Cart(1L, "2", "02", new BigInteger("2000"));
-		cart.calculate();
-		System.out.println("Price: " + cart.getPrice());
-		System.out.println("Price IVA: " + cart.getPriceIva());
-		System.out.println("Price + Price IVA: " + cart.calculatePriceAddPriceIva());
-		System.out.println("Discount: " + cart.getPriceDiscount());
-		System.out.println("Price - Discount: " + cart.calculatePriceSubDiscount());
-		System.out.println("Subtotal Bruto: " + cart.getSubtotalGross());
-		System.out.println("Subtotal Neto: " + cart.getSubtotalNet());
-	}
-
-	///////////////////////////////////////////////////////
-	// Method - Private
-	///////////////////////////////////////////////////////
-	private BigInteger calculateToString(String chain) {
-		if (PastleyValidate.isChain(chain)) {
-			BigDecimal price = new BigDecimal(this.price);
-			int number = (PastleyValidate.isNumber(chain)) ? Integer.parseInt(chain) : 0;
-			price = price.multiply(new BigDecimal((double) (number / 100d)));
-			return price.toBigInteger();
-		}
-		return BigInteger.ZERO;
-	}
 
 	///////////////////////////////////////////////////////
 	// Getter and Setters
@@ -213,14 +119,6 @@ public class Cart implements Serializable {
 		return count;
 	}
 
-	public BigInteger getPriceIva() {
-		return priceIva;
-	}
-
-	public void setPriceIva(BigInteger priceIva) {
-		this.priceIva = priceIva;
-	}
-
 	public void setCount(int count) {
 		this.count = count;
 	}
@@ -251,14 +149,6 @@ public class Cart implements Serializable {
 
 	public void setSubtotalNet(BigInteger subtotalNet) {
 		this.subtotalNet = subtotalNet;
-	}
-
-	public BigInteger getPriceDiscount() {
-		return priceDiscount;
-	}
-
-	public void setPriceDiscount(BigInteger priceDiscount) {
-		this.priceDiscount = priceDiscount;
 	}
 
 	public BigInteger getSubtotalGross() {
