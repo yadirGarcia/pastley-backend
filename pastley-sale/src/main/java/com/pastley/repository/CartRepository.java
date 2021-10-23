36f35a524d9e226a7a1d6a4531795ef669c7a017
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-10-23 06:42:17 -0500

    Add CartRepository

--- pastley-sale/src/main/java/com/pastley/repository/CartRepository.java
+++ pastley-sale/src/main/java/com/pastley/repository/CartRepository.java
@@ -1,11 +1,9 @@
 package com.pastley.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pastley.entity.SaleDetail;
+import com.pastley.entity.Cart;
 
 /**
  * @project Pastley-Sale.
@@ -15,12 +13,6 @@ import com.pastley.entity.SaleDetail;
  * @version 1.0.0.
  */
 @Repository
-public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long>{
-	
-	/**
-	 * Method that allows knowing the sale details of a sale made.
-	 * @param sale, Represents the sale.
-	 * @return List with sale details.
-	 */
-	public List<SaleDetail> findByIdSale(Long sale);
+public interface CartRepository extends JpaRepository<Cart, Long>{
+
 }
