0fb94ddcc434285ddadab6c5dde572131a73b288
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-11-02 07:51:03 -0500

    Config feingclient person in sale

--- pastley-sale/src/main/java/com/pastley/feignclients/PersonFeignClient.java
+++ pastley-sale/src/main/java/com/pastley/feignclients/PersonFeignClient.java
@@ -2,14 +2,15 @@ package com.pastley.feignclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pastley.model.PersonModel;
 
-@FeignClient(name = "pastley-user", url = "http://localhost:8080")
+@FeignClient(name = "pastley-user")
 @RequestMapping("/person")
 public interface PersonFeignClient {
 
-	@GetMapping
-	public PersonModel findByDocument(Long document);
+	@GetMapping(value = { "/findByDocument/{document}" })
+	public PersonModel findByDocument(@PathVariable("document") Long document);
 }
