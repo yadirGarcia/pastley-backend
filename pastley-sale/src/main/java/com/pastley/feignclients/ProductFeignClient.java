7c37c4e3cbfe208f35a5a500805d78d0170a9174
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-11-02 16:34:26 -0500

    Interface product and person -> sale

--- pastley-sale/src/main/java/com/pastley/feignclients/ProductFeignClient.java
+++ pastley-sale/src/main/java/com/pastley/feignclients/ProductFeignClient.java
@@ -1,11 +1,14 @@
 package com.pastley.feignclients;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.pastley.model.PersonModel;
+import com.pastley.model.ProductModel;
 
 /**
  * @project Pastley-Sale.
@@ -14,10 +17,13 @@ import com.pastley.model.PersonModel;
  * @contributors soleimygomez, leynerjoseoa, jhonatanbeltran.
  * @version 1.0.0.
  */
-@FeignClient(name = "pastley-user")
-@RequestMapping("/person")
-public interface PersonFeignClient {
-
-	@GetMapping(value = { "/findByDocument/{document}" })
-	public PersonModel findByDocument(@PathVariable("document") Long document);
+@FeignClient(name = "pastley-product")
+@RequestMapping("/product")
+public interface ProductFeignClient {
+	
+	@GetMapping(value = { "/findById/{id}" })
+	public ProductModel findById(@PathVariable("id") Long id);
+	
+	@PutMapping(value = {"update/stock"})
+	public ProductModel updateStock(@RequestBody ProductModel product);
 }
