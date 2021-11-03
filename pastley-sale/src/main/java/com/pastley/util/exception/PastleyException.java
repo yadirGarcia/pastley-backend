02308f8c806015353bddee28307a7073498d3c0b
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-11-03 08:10:59 -0500

    Exception Main Sale

--- pastley-sale/src/main/java/com/pastley/util/exception/PastleyException.java
+++ pastley-sale/src/main/java/com/pastley/util/exception/PastleyException.java
@@ -1,5 +1,40 @@
 package com.pastley.util.exception;
 
-public class PastleyException {
+import org.springframework.http.HttpStatus;
+
+/**
+ * @project Pastley-Sale.
+ * @author Sergio Stives Barrios Buitrago.
+ * @Github https://github.com/SerBuitrago.
+ * @contributors soleimygomez, leynerjoseoa, jhonatanbeltran.
+ * @version 1.0.0.
+ */
+public class PastleyException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	private HttpStatus httpStatus;
+
+	///////////////////////////////////////////////////////
+	// Builders
+	///////////////////////////////////////////////////////
+	public PastleyException(HttpStatus httpStatus, String message) {
+		super(message);
+		this.httpStatus = httpStatus;
+	}
+
+	///////////////////////////////////////////////////////
+	// Getter and Setetrs
+	///////////////////////////////////////////////////////
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+
+	public void setHttpStatus(HttpStatus httpStatus) {
+		this.httpStatus = httpStatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
